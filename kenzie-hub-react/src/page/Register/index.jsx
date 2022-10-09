dd6398ab367031bd614c7f04c89afc0083c869a6
cauesantg <caue_paraty@hotmail.com>
2022-10-09 13:01:53 -0300

    feat: add routes

---
+++ kenzie-hub-react/src/page/Register/index.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom"
+
+const Register = () => {
+
+    
+    return (
+        <>
+        <Link to="/login">login</Link>
+        <Link to="/dashboard">Dashboard</Link>
+
+        
+        </>
+    )
+}
+export default Register
---
+++ kenzie-hub-react/src/routes/index.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+
+import Register from "../page/Register";
+import Login from "../page/Login";
+import Dashboard from "../page/Dashboard";
+
+const RoutesMain = () => (
+  
+  <Routes>
+    <Route path="/" element={<Register />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/dashboard" element={<Dashboard />} />
+  </Routes>
+);
+
+export default RoutesMain;
